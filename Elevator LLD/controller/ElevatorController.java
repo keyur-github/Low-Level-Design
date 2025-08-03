35f8f0e1607966a5fb21816edb76691a27bd6d8f
Keyur <keyur.np@somaiya.edu>
2025-08-03 18:12:06 +0530

    Added requestElevator and processRequest method

--- Elevator LLD/controller/ElevatorController.java
+++ Elevator LLD/controller/ElevatorController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import entity.Elevator;
+import entity.Request;
+
+import java.util.*;
+
 public class ElevatorController {
+
+    private final List<Elevator> elevators = new ArrayList<>();
+
+    public ElevatorController(Integer numElevators, Integer minFloor, Integer maxFloor) {
+        for(int i = 1; i <= numElevators; i++) {
+            elevators.add(new Elevator(i, minFloor, maxFloor));
+        }
+    }
+
+    public void requestElevator(Request request) {
+        Elevator chosenElevator = Collections.min(
+                elevators,
+                Comparator.comparingInt(x -> Math.abs(x.getCurrentFloor()-request.getFloor()))
+        );
+        chosenElevator.addRequests(request);
+    }
+
+    public void processRequest() {
+
+    }
+
+
 }
